e9358d5edc1dd14c21d8cf5f5874ba6af5649304
itechdom <oalghnmi@gmail.com>
2019-11-05 09:54:39 -0500

    remvoe node modules

--- web/node_modules/orbital-react-services/react-views-service/react-views-service.js
+++
@@ -1,4 +0,0 @@
-import * as Material from "../../react-views-service/lib/material";
-import rootStore from "../../react-views-service/lib/store";
-import * as Models from "../../react-views-service/lib/models";
-export { Models, Material, rootStore };
